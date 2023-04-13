0dc88bc958e93fdd2095e1a8ee4e465ed2633269
Lalith Kumar <lalithbarani25@gmail.com>
2023-04-13 18:27:26 +0530

    Conflict Resolved

--- src/modules/customer-setup/tabs/api-setup/api-setup-tabs.js
+++ src/modules/customer-setup/tabs/api-setup/api-setup-tabs.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-import { Card, Col, Row ,Switch} from "antd";
-=======
 import { Card, Col, Row, Switch } from "antd";
->>>>>>> e63602a216da12a712559aa20dbb593466e29333
 import React, { Fragment } from "react";
 import PrimaryLabel from "../../../../component/labels/primary-label";
 import FormInput from "../../../../component/form-input/form-input";
@@ -15,18 +11,11 @@ import { useSelector } from "react-redux";
 
 
 function ApiSetupTabs() {
-<<<<<<< HEAD
-
-  const onChangeChecked = (checked) => {
-    console.log(`switch to ${checked}`);
-};
-=======
   const show = useSelector(state => state.menubar.menuVisible);
   const onChange = (checked) => {
     console.log(`switch to ${checked}`);
 
   }
->>>>>>> e63602a216da12a712559aa20dbb593466e29333
 
   return (
     <Fragment>
@@ -70,64 +59,6 @@ function ApiSetupTabs() {
           <Col sm={24}>
             <PrimaryLabel>What is the purpose of account?</PrimaryLabel>
 
-<<<<<<< HEAD
-        </Col>
-      </Row>
-      <Row className="mt-15 align-items-center " gutter={5}>
-        <Col sm={2}>
-          <PathIcon />
-        </Col>
-        <Col sm={18} className="normal-text">
-          <PrimaryLabel normal={true}>Activate for Tracking</PrimaryLabel>
-        </Col>
-        <Col sm={4} className="text-right">
-        <Switch defaultChecked onChange={onChangeChecked} />
-        </Col>
-      </Row>
-      <Row className="mt-15" gutter={12}>
-        <Col sm={24}>
-          <FormInput
-            editMode={true}
-            placeholder={"Tracking or Pro Number"}
-            name="trackingNumber"
-          />
-        </Col>
-
-      </Row>
-      <Row className="mt-15 align-items-center " gutter={5}>
-        <Col sm={2}>
-          <ShieldIcon />
-        </Col>
-        <Col sm={18} className="normal-text">
-          <PrimaryLabel normal={true}>Activate for Rate</PrimaryLabel>
-        </Col>
-        <Col sm={4} className="text-right">
-        <Switch defaultChecked onChange={onChangeChecked} />
-        </Col>
-      </Row>
-      <Row className="mt-15 align-items-center " gutter={5}>
-        <Col sm={2}>
-          <PackageIcon />
-        </Col>
-        <Col sm={18} className="normal-text">
-          <PrimaryLabel normal={true}>Activate for Pickup Request</PrimaryLabel>
-        </Col>
-        <Col sm={4} className="text-right">
-        <Switch defaultChecked onChange={onChangeChecked} />
-        </Col>
-      </Row>
-      <Row className="mt-15 align-items-center " gutter={5}>
-        <Col sm={2}>
-          <ImageIcon />
-        </Col>
-        <Col sm={18} className="normal-text">
-          <PrimaryLabel normal={true}>Activate for Image</PrimaryLabel>
-        </Col>
-        <Col sm={4} className="text-right">
-        <Switch defaultChecked onChange={onChangeChecked} />
-        </Col>
-      </Row>
-=======
           </Col>
         </Row>
         <Row className="mt-5 align-items-center " gutter={5}>
@@ -185,7 +116,6 @@ function ApiSetupTabs() {
           </Col>
         </Row>
       </div>
->>>>>>> e63602a216da12a712559aa20dbb593466e29333
     </Fragment>
   );
 }
